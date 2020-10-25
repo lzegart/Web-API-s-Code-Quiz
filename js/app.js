c9ef94c5167601ede01b9febb353c53ad4a58973
LZ <laraezegart@gmail.com>
2020-10-24 17:02:08 -0700

    added high scores btn, and timer, and hid end element

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-var startNowEl = document.getElementById("start-now")
-    startNowEl.addEventListener("click", function(event){
-        console.log("Hiiii")
-        console.log(event.target.textContent)
-    })
+// var questionList = document.getElementById(questionList) 
+// // var event = prompt(questionList)
+// var startNowEl = document.getElementById("start-element")
+//     startNowEl.addEventListener("click", function(event){
+//         // console.log("Starting...")
+//         console.log(event.target.textContent)
+//     })
+ 
 
-    
+var timerCount = 50
+var timerEl = document.querySelector("#time")
+
+timerEl.textContent = timerCount
+
+var timer = window.setInterval(function() {
+    timerCount--;
+    timerEl.textContent = timerCount
+
+    if (timerCount === 0) {
+        clearInterval(timer)
+    }
+}, 500)
