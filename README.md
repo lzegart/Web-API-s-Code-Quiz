96484ac3d40fed278a6f8a3b624080d2ec127bb4
LZ <laraezegart@gmail.com>
2020-10-30 14:10:46 -0700

    started readMe and timer subtracts on wrong answers

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Web-API-s-Code-Quiz
+
+This code quiz allows the user to take a short multiple choice quiz to test their knowledge.
+The quiz is timed, and the score is the time the user has left.
+If you miss an answer, time is subtracted from the timer.
+Once completed, the score is displayed and you can enter initials to save to a local storage file.  
--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var questionsEl = document.querySelector("#questions")
 var endEl = document.querySelector("#end-element")
 var spanScoreEl = document.querySelector("#score")
-var finishedQ = false 
+var finishedQ = false
 var currentHighScore = 0
 var timerCount = 50
 
 // function with timer, show questions and hide start-element
 var startBtn = document.querySelector("#start-now")
-startBtn.addEventListener("click", function () { 
+startBtn.addEventListener("click", function () {
 
     var timerEl = document.querySelector("#time")
 
     timerEl.textContent = timerCount
 
-    var timer = window.setInterval(function() {
+    var timer = window.setInterval(function () {
         timerCount--;
         timerEl.textContent = timerCount
 
-        if (timerCount === 0) {
+        if (timerCount <= 0) {
+            timerCount = 0;
+            timerEl.textContent = 0;
             clearInterval(timer)
             alert("Time is up!")
         } else if (finishedQ) {
@@ -28,7 +30,7 @@ startBtn.addEventListener("click", function () {
         }
     }, 1000)
 
-    var startEl  = document.querySelector("#start-element")
+    var startEl = document.querySelector("#start-element")
     startEl.classList.add("hide");
     questionsEl.classList.remove("hide");
 
@@ -49,48 +51,74 @@ function displayQuestion() {
     var option3El = document.querySelector("#option3")
     option3El.textContent = questions[currentQ].choices[3]
 
-    currentQ +=1
+
 }
 
+var feedbackEl = document.querySelector("#answer-eval")
+
 var choicesList = document.querySelector("#choices")
-choicesList.addEventListener("click", function(event) {
-    console.log(event)
+choicesList.addEventListener("click", function (event) {
+    //console.log(event)
     var eventChoice = event.target
-    console.log(eventChoice.matches("button"))
-    if (eventChoice.matches("button")) {
-        // if ()
+    //console.log(eventChoice)
+    console.log(eventChoice.innerText === questions[currentQ].a)
+    if (eventChoice.innerText !== questions[currentQ].a) {
+        timerCount -= 10;
+        // if (timerCount < 0) {
+        //     timerCount = 0;
+        // }
+        feedbackEl.textContent = "Wrong!";
+    } else {
+        feedbackEl.textContent = "Correct!";
+    }
+    // document.querySelector("#feedback-element").setAttribute("class", "show")
+    currentQ += 1;
+
+    setTimeout(function () {
+        // document.querySelector("#feedback-element").setAttribute("class", "hide")
         if (questions.length > currentQ) {
-            displayQuestion() 
+            displayQuestion()
         } else {
             console.log("finished")
             questionsEl.classList.add("hide");
             endEl.classList.remove("hide");
             finishedQ = true
-        }        
-    }
+        }
+    }, 1000);
+    // }
 })
 
-// var userInitials = [];
 var inputText = document.querySelector("#input-form");
 var submitInitials = document.querySelector("#submit");
-// inputText.push(initialsChoices)
 
 var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
 var newScore = {
-    score: time 
-    // initials: initials
+    score: time
 };
-submitInitials.addEventListener("click", function() {
-    // e.preventDefault()
-    highScores.push(newScore)
-    console.log(inputText.value)
-    window.localStorage.setItem("initials", JSON.stringify(inputText));
-    window.localStorage.setItem("score", JSON.stringify(currentHighScore))
-    console.log(currentHighScore)
+
+
+submitInitials.addEventListener("click", function () {
+    var newScoreObj = {
+        "inititals": inputText.value,
+        "score": timerCount
+    }
+    highScores.push(newScoreObj)
+    //console.log(inputText.value)
+    window.localStorage.setItem("highScores", JSON.stringify(highScores))
+    // window.localStorage.setItem("initials", JSON.stringify(inputText.value));
+    // window.localStorage.setItem("score", JSON.stringify(currentHighScore))
+    // console.log(currentHighScore)
 })
 
 var highScoreList = document.querySelector("#high-score-btn")
 highScoreList.addEventListener("click", function (event) {
-    console.log("scores")
-    alert(highScoreList)
+    // console.log("scores")
+    // alert(highScoreList)
+    // window.localStorage.getItem("initials", JSON.stringify(inputText.value));
+    // window.localStorage.getItem("score", JSON.stringify(currentHighScore));
+ 
+    var highScoresArray = JSON.parse(window.localStorage.getItem("highScores")) || [];
+    console.log(highScoresArray)
+
+    // TODO: use a for loop to display all the scores and append them to the html
 })
